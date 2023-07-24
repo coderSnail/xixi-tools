e0ea31d971f7a4f65e5fba8e3369f42069d53c23
CoderSnail <1418877185@qq.com>
2023-07-25 02:28:34 +0800

    自动匹配文件模板;日期检查;

--- view/file_item_view.py
+++ view/file_item_view.py
@@ -4,12 +4,16 @@ from ui.file_item_ui import Ui_file_item
 
 
 class FileItemView(QWidget, Ui_file_item):
-    def __init__(self, parent=None, file=None, data_configs=None):
+    def __init__(self, parent=None, file=None, data_config=None):
         super().__init__(parent=parent)
         self.setupUi(self)
         self.file = file
-        self.data_configs = data_configs
+        self.data_config = data_config
 
-        print(self.file, self.data_configs)
         self.label_file_name.setText(self.file)
-        self.combo_file_type.addItems(self.data_configs)
+        # 设置文件类型下拉框
+        self.combo_file_type.addItems(list(self.data_config.keys()))
+        # 自动识别文件类型
+        for key in self.data_config.keys():
+            if file.upper().__contains__(self.data_config[key]['channel_id'].upper()) and file.upper().__contains__(self.data_config[key]['type_id'].upper()):
+                self.combo_file_type.setCurrentText(key)
--- view/home_view.py
+++ view/home_view.py
@@ -2,9 +2,9 @@ import calendar
 from datetime import datetime
 from enum import Enum
 
-from PyQt6.QtCore import QDate, pyqtSignal, QFileSystemWatcher
+from PyQt6.QtCore import QDate, pyqtSignal, QFileSystemWatcher, Qt
 from PyQt6.QtWidgets import QWidget, QFileDialog, QListWidgetItem
-from qfluentwidgets import ToolTipFilter, ToolTipPosition, TeachingTip, InfoBarIcon, TeachingTipTailPosition
+from qfluentwidgets import ToolTipFilter, ToolTipPosition, TeachingTip, InfoBarIcon, TeachingTipTailPosition, InfoBar, InfoBarPosition
 
 from ui.home_ui import Ui_home_widget
 from util import common_util
@@ -21,10 +21,17 @@ class HomeView(QWidget, Ui_home_widget):
         self.setupUi(self)
 
         self.status = Status.INIT
+        self.start_date = None  # 开始日期
+        self.end_date = None  # 结束日期
+
         self.data_dir = ''  # 源数据表文件路径
         self.data_files = []  # 源数据表
-        self.companies = []  # 分析后所有源数据表存在的集团名(公司映射后的)
+        self.data_config = common_util.load_data_config()  # 源数据表配置
+        self.file_item_widgets = []  # 文件列表行控件
+
         self.companies_checked = []  # 选中的集团名
+        self.companies = []  # 分析后所有源数据表存在的集团名(公司映射后的)
+        self.company_config = common_util.load_company_config()  # 源数据表配置
 
         self.data_config_watcher = QFileSystemWatcher([common_util.get_real_path('config', 'data_config.json')])
         self.data_config_watcher.fileChanged.connect(self.data_config_changed)
@@ -49,21 +56,43 @@ class HomeView(QWidget, Ui_home_widget):
 
     def analyse_data(self):
         # todo: 校验文件与模板是否匹配, 时间格式是否能正确解析
-        self.status_signal.emit(Status.ANALYSED)
+        # a 检查所有文件是否都配置了模板
+        flag = True
+        for file_item_widget in self.file_item_widgets:
+            if not file_item_widget.combo_file_type.text():
+                flag = False
+                break
+        if flag:
+            # a.1 配置了模板的进一步检查模板是否匹配
+
+            # b 根据模板和源数据表配置, 解析文件包含的所有 集团-公司
+
+            self.status_signal.emit(Status.ANALYSED)
+        else:
+            # a.2 未配置模板的文件弹窗提示
+            InfoBar.error(
+                title='文件列表错误',
+                content="每行必须选择一个配置!\n如无合适配置请先在源数据表配置中新增一条!",
+                orient=Qt.Orientation.Horizontal,
+                isClosable=False,  # disable close button
+                position=InfoBarPosition.TOP,
+                duration=2000,
+                parent=self
+            )
 
     def import_data(self):
+        self.status_signal.emit(Status.INIT)
         files, _ = QFileDialog.getOpenFileNames(self, '导入源数据表', r'D:\00-ZJPC\桌面\数据需求', 'Excel文件(*.xlsx)')
         if files:
             self.label_data_dir.setText('/'.join(files[0].split('/')[:-1]))
             self.data_files = [file.split('/')[-1] for file in files]
 
             self.list_widget_file.clear()
-            # self.list_widget_file.addItems(self.data_files)
-            for file in self.data_files:
+            for data_file in self.data_files:
                 item = QListWidgetItem(self.list_widget_file)
-                # todo 自动识别模板
-                item_widget = FileItemView(file=file, data_configs=['1', '华为消耗', 'oppo消耗'])
+                item_widget = FileItemView(file=data_file, data_config=self.data_config)
                 self.list_widget_file.setItemWidget(item, item_widget)
+                self.file_item_widgets.append(item_widget)
 
             self.status_signal.emit(Status.IMPORTED)
 
@@ -78,6 +107,14 @@ class HomeView(QWidget, Ui_home_widget):
     def date_changed(self):
         """ 检查日期范围的有效性, 并弹出可关闭提示框 """
         if self.calendar_start.date.daysTo(self.calendar_end.date) < 0:
+            if self.start_date:
+                self.calendar_start.dateChanged.disconnect(self.date_changed)
+                self.calendar_start.setDate(self.start_date)
+                self.calendar_start.dateChanged.connect(self.date_changed)
+            if self.end_date:
+                self.calendar_end.dateChanged.disconnect(self.date_changed)
+                self.calendar_end.setDate(self.end_date)
+                self.calendar_end.dateChanged.connect(self.date_changed)
             TeachingTip.create(
                 target=self.calendar_end,
                 icon=InfoBarIcon.ERROR,
@@ -88,6 +125,9 @@ class HomeView(QWidget, Ui_home_widget):
                 duration=5000,
                 parent=self
             )
+        else:
+            self.start_date = self.calendar_start.date
+            self.end_date = self.calendar_end.date
 
     def combo_month_changed(self):
         """ 快捷选择月份下拉框触发时, 自动选择对应月份的第一天和最后一天 """
@@ -113,7 +153,7 @@ class HomeView(QWidget, Ui_home_widget):
         if status == Status.IMPORTED:
             self.set_status(analyse=True, export=False, clear_data_file=False, clear_data_company=True)
         if status == Status.ANALYSED:
-            self.set_status(analyse=False, export=True, clear_data_file=False, clear_data_company=False)
+            self.set_status(analyse=True, export=True, clear_data_file=False, clear_data_company=False)
         if status == Status.MODIFIED:
             self.set_status(analyse=True, export=False, clear_data_file=True, clear_data_company=True)
 
@@ -125,10 +165,13 @@ class HomeView(QWidget, Ui_home_widget):
         if clear_data_file:
             self.data_dir = ''
             self.label_data_dir.setText('')
-            self.data_files = []
             self.list_widget_file.clear()
+            self.data_files = []
+            self.data_config = common_util.load_data_config()
+            self.file_item_widgets = []
         if clear_data_company:
             self.check_box_select_all.setChecked(False)
+            self.list_widget_company.clear()
             self.companies = []
             self.companies_checked = []
-            self.list_widget_company.clear()
+            self.company_config = common_util.load_company_config()
